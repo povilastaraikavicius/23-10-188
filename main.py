e50578f5816151934735f94ecea07084b03da3ef
povilas taraikavicius <ptpecka@gmail.com>
2023-10-19 09:55:03 +0300

    add lesson

---
+++ main.py
@@ -0,0 +1,69 @@
+# a=5
+# b=6
+
+# def add_two_numbers(a, b):
+#     print(a + b)
+
+# add_two_numbers(5,8)
+
+# def add_two_numbers(a, b):
+#     return(a + b)
+# nr_sum = add_two_numbers(412, 215)
+# print(nr_sum)
+
+
+# Create a function that takes string as parameter and returns number of letters from that string.
+
+# def count_letters(input_string):
+#     caunt_leters = 0
+#     for char in input_string:
+#      if char.isalpha():
+#       caunt_leters +=1 
+
+
+# def count_letters(in_string: str):    
+#     return len(in_string.replace(" ", ""))
+
+
+# def add_two_numbers(a: int, b:int) ->int:
+#     return(a + b)
+
+# def square_two_numbers(a: int)-> int:
+#     return a ** 2
+
+# def make_a_string(text: str,x: int)->str:
+#     number = square_two_numbers (x)
+#     return text + str(number)
+# print(make_a_string("betkas",5))
+
+
+def divide_numbers(a: int ) -> int:
+    return a // 2 
+# number = divide_numbers(40 )
+# print(number)
+
+def make_a_string(text: str,x: int)->str:
+    number = divide_numbers (x)
+    return text + str(number)
+print(make_a_string("betkas",80))
+
+def sum_number(a: int) -> int:
+    return a + 15 
+number = sum_number(33)
+print(number)
+
+
+
+# Create a function that adds a string ending to each member in a list.
+# Sukurkite funkciją, kuri prie kiekvieno sąrašo nario prideda eilutės galūnę.
+
+
+# def sum_text(text: str ) -> str:
+#     a = sum_text ()
+#     return text +(a)
+
+# a = sum_text ()
+# print (a)
+
+
+
